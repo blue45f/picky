03694e80cf77b4323e5c88fec9afc1bf65696ad2
hjunkim <blue45f@gmail.com>
2026-06-16 20:28:47 +0900

    fix: route all API paths through single Vercel function

--- api/auth/guest.ts
+++
@@ -1,6 +0,0 @@
-import { getApiRequestHandler } from '../_handler';
-
-export default async function handler(req: any, res: any) {
-  const handler = await getApiRequestHandler();
-  return handler(req, res);
-}
